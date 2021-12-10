d356890c2fd0c8e28d6f56bcecccc894dea16600
JJJackal <82609492+JJJackal@users.noreply.github.com>
2021-12-09 17:44:30 -0800

    connecting component to state and reducers

--- tetris/src/components/Controls.js
+++ tetris/src/components/Controls.js
@@ -1,30 +1,32 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 export default function Controls(props) {
+    const dispatch = useDispatch();
+    const isRunning = useSelector((state) => state.isRunning);
+
     return (
-        <div className='controls'>
-
-
-             {/* left */}
-             <button className="control-button" onClick={(e) => {
-
-}}>Left</button>
+        <div className="controls">
+            {/* left */}
+            <button className="control-button" onClick={(e) => {
+                dispatch(moveLeft())
+            }}>Left</button>
 
             {/* right */}
             <button className="control-button" onClick={(e) => {
-
-}}>Right</button>
+                dispatch(moveRight())
+            }}>Right</button>
 
             {/* rotate */}
             <button className="control-button" onClick={(e) => {
-
-}}>Rotate</button>
+                dispatch(rotate())
+            }}>Rotate</button>
 
             {/* down */}
             <button className="control-button" onClick={(e) => {
-
-}}>Down</button>
-
+                dispatch(moveDown())
+            }}>Down</button>
 
         </div>
     )
