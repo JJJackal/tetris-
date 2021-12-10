6919acdd32bff3897db21c0b4ed6f8794b6a9331
JJJackal <82609492+JJJackal@users.noreply.github.com>
2021-12-09 17:11:05 -0800

    display next shape implementation

--- tetris/src/components/NextBlock.js
+++ tetris/src/components/NextBlock.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { shapes } from '../utils';
 import GridSquare from './GridSquare';
 
 //Draws the 'next' block view showing the next block to drop 
 
 export default function NextBlock(props) {
+    const nextShape = useSelector((state) => state.game.nextShape);
+    const box = shapes[nextShape][0]; //getthe first rotation
 
-    const box =[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    // const box =[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
     //Map the block to the grud
     const grid = box.map((rowArray, row) => {
         return rowArray.map((square, col) => {
